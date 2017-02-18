14a00281d3344c78f735320ed51a68d93434006e
sujit0892 <sujitkumar0892@gmail.com>
2017-02-18 19:55:38 +0530

    10. commit

---
+++ ttoast/app/src/main/java/com/example/marinex/ttoast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.marinex.ttoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast=Toast.makeText(getApplicationContext(),"toast",Toast.LENGTH_LONG);
+                toast.show();
+                toast.setGravity(Gravity.CENTER,0,0);
+            }
+        });
+    }
+}
