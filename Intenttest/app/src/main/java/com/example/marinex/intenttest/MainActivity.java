116ecaf9935e9a518e14f919ede0b1c9796e63a6
sujit0892 <sujitkumar0892@gmail.com>
2017-02-18 11:17:26 +0530

    sixth commit

---
+++ Intenttest/app/src/main/java/com/example/marinex/intenttest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.marinex.intenttest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
